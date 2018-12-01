8c8e58ff4c97ed715eec0697a801056cb3c35eab
Ali Kheradmand <a.i.kheradmand@gmail.com>
2018-11-30 19:49:39 -0600

    fix inefficiencies

--- src/astar_aligner.h
+++ src/astar_aligner.h
@@ -9,6 +9,7 @@
 #define SRC_EFFICIENT_ALIGNER_H_
 
 #include "aligner.h"
+#include "carrillo_lipman.h"
 #include <set>
 #include <map>
 #include <queue>
@@ -41,46 +42,12 @@ private:
 
 	typedef std::unordered_map<index_t, index_t> closedset_t; //= closed set
 
-	score_t get_optimal_pairwise_alignment_score(const sequence_t& s1, const std::size_t p1, const sequence_t& s2, const std::size_t p2){
-		//std::cerr << "p1 is " << p1 << " and p2 is " << p2 << std::endl;
-		const auto subseq1_len = s1.length() + 1 - p1;
-		const auto subseq2_len = s2.length() + 1 - p2;
-		std::vector<score_t> best_scores(subseq2_len + 1);
-		best_scores[0] = 0;
-		// i = 0;
-		for (auto j = 1; j <= subseq2_len; j++)
-			best_scores[j] = best_scores[j-1] + scoring.score(GAP, s2[j + p2 - 2]);
-		for (auto i = 1 ; i <= subseq1_len; i++){
-			//j=0
-			score_t prev_row_best_score = best_scores[0];
-			best_scores[0] = best_scores[0] + scoring.score(s1[i + p1 - 2], GAP);
-			for (auto j = 1; j <= subseq2_len; j++){
-				const score_t _c = best_scores[j - 1] + scoring.score(GAP, s2[j + p2 - 2]);
-				const score_t c_ = best_scores[j] + scoring.score(s1[i + p1 - 2], GAP);
-				const score_t cc = prev_row_best_score + scoring.score(s1[i + p1 - 2], s2[j + p2 - 2]);
-				prev_row_best_score = best_scores[j];
-				best_scores[j] = std::max(cc, std::max(_c,c_));
-				//std::cerr << "best_score(" << i << "," << j << ") is " <<  best_scores[j] << std::endl;
-			}
-		}
-		//std::cerr << "optimal alignment for " << s1.substr(p1-1, s1.length() + 1 - p1) << " and  " << s2.substr(p2-1, s2.length() + 1 - p2) << " is " << best_scores[subseq2_len] << std::endl;
-		return best_scores[subseq2_len];
-	}
-
-	score_t get_carrillo_lipman_score(const sequences_t& seqs, const positions_t& pos){
-		score_t ret;
-		for (auto i = 0; i < seqs.size(); i++){
-			for (auto j = i + 1; j < seqs.size(); j++){
-				ret += get_optimal_pairwise_alignment_score(seqs[i], pos[i] + 1, seqs[j], pos[j] + 1);
-			}
-		}
-		return ret;
-	}
 
 public:
 	astar_aligner_t(scoring_function_t& _scoring) : aligner_t(_scoring){}
 
 	std::pair<sequences_t,score_t> get_alignment(const sequences_t& seqs) {
+		carrillo_lipman_score_t carillo_lipman_score(seqs, scoring);
 		std::cerr << "starting to align" << std::endl;
 		position_util_t pu(seqs);
 		openset_t openset;
@@ -103,9 +70,10 @@ public:
 
 		chars_t chars(dims);
 		positions_t neighbor_pos(dims);
+		std::size_t visited = 0;
 		while (true) {
+			++visited;
 			assert(not openset.empty());
-
 			const auto node = openset.top();
 			openset.pop();
 			if (parents.find(node.index) != parents.end())
@@ -131,7 +99,7 @@ public:
 				//std::cerr << "at neighbor index " << neighbor_index << std::endl;
 
 				pu.convert_back(neighbor_index, neighbor_pos);
-				const auto cl_score = get_carrillo_lipman_score(seqs, neighbor_pos);
+				const auto cl_score = carillo_lipman_score.get_score(seqs, neighbor_pos);
 				const auto sp_score = scoring.sp_score(chars);
 				openset.emplace(node_t{neighbor_index, node.f + sp_score, cl_score, permutation});
 			}
@@ -139,9 +107,12 @@ public:
 
 		assert (parents.find(last_index) != parents.end());
 
+		std::cerr << "open set " << openset.size() << " closed set " << parents.size() << " sum " << openset.size() + parents.size() << std::endl;
+		std::cerr << "space size " << pu.space_size() << " visited " << visited <<  std::endl;
+
 		sequences_t alignment{dims};
 
-		//td::cerr << "best score is " << best_score << std::endl;
+		//std::cerr << "best score is " << best_score << std::endl;
 		//std::cerr << "traceback to construct an alignment" << std::endl;
 
 		//traceback
---
+++ src/carrillo_lipman.h
@@ -0,0 +1,95 @@
+/*
+ * carrillo_lipman.h
+ *
+ *  Created on: Nov 30, 2018
+ *      Author: ali
+ */
+
+#ifndef SRC_CARRILLO_LIPMAN_H_
+#define SRC_CARRILLO_LIPMAN_H_
+
+#include "score.h"
+
+class carrillo_lipman_score_t {
+	scoring_function_t& scoring;
+	score_t** scores;
+	std::size_t dims;
+
+public:
+	carrillo_lipman_score_t(const sequences_t& seqs, scoring_function_t& _scoring) : scoring(_scoring){
+		dims = seqs.size();
+		assert(dims > 1);
+		scores = new score_t*[dims * dims];
+
+		for (auto i = 0; i < dims; i++){
+			for (auto j = i + 1; j < dims; j++){
+				scores[ i * dims + j ] = compute_pairwise_backward_alignment_scores(seqs[i], seqs[j]);
+			}
+		}
+	}
+
+	~carrillo_lipman_score_t(){
+		for (auto i = 0; i < ((dims - 1) * dims) / 2; i++){
+			delete scores[i];
+		}
+		delete scores;
+	}
+
+	inline std::size_t convert(const sequence_t& a, int i, int j){
+		return j * (a.size() + 1) + i;
+	}
+
+	score_t* compute_pairwise_backward_alignment_scores(const sequence_t& a, const sequence_t& b){
+		score_t* best_scores = new score_t[(a.size() + 1) * (b.size() + 1)];
+
+		best_scores[convert(a, a.size(), b.size())] = 0;
+
+		//i = a.size()
+		for (int j = b.size() - 1; j >= 0 ; j--)
+			best_scores[convert(a, a.size(), j)] = best_scores[convert(a, a.size(), j + 1)] + scoring.score(GAP, b[j]);
+		for (int i = a.size() - 1; i >= 0; i--){
+			//j = b.size()
+			best_scores[convert(a, i, b.size())] = best_scores[convert(a, i + 1, b.size())] + scoring.score(a[i], GAP);
+			for (int j = b.size() - 1; j >= 0; j--){
+				const auto cc = best_scores[convert(a, i + 1, j + 1)] + scoring.score(a[i], b[j]);
+				const auto c_ = best_scores[convert(a, i + 1, j)] + scoring.score(a[i], GAP);
+				const auto _c = best_scores[convert(a, i, j + 1)] + scoring.score(GAP, b[j]);
+				best_scores[convert(a, i, j)] = std::max(cc, std::max(_c,c_));
+			}
+		}
+
+		//print_best_scores(a, b, best_scores);
+
+		return best_scores;
+	}
+
+	void print_best_scores(const sequence_t& a, const sequence_t& b, score_t* best_scores){
+		for (int i = 0 ; i <= a.size(); i++){
+			for (int j = 0; j <= b.size(); j++){
+				std::cerr << best_scores[convert(a, i, j)] << "\t";
+			}
+			std::cerr << std::endl;
+		}
+		std::cerr << std::endl;
+	}
+
+
+
+	score_t get_score(const sequences_t& seqs, const positions_t& pos){
+		//std::cerr << "pos is " << pos << std::endl;
+		score_t ret = 0;
+		for (auto i = 0; i < dims; i++){
+			for (auto j = i + 1; j < dims; j++){
+				const auto score = scores[ i * dims + j ][convert(seqs[i], pos[i], pos[j])];
+				//std::cerr << "optimal alignment for " << seqs[i].substr(pos[i], seqs[i].length() - pos[i]) << " and  " << seqs[j].substr(pos[j], seqs[j].length() - pos[j]) << " is " << score  << std::endl;
+				ret += score;
+			}
+		}
+		return ret;
+	}
+
+};
+
+
+
+#endif /* SRC_CARRILLO_LIPMAN_H_ */
